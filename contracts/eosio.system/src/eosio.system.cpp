d41054f2dbaf12492ac5acff02a5c13c0d1368ee
Quoc Le <songmai108@gmail.com>
2020-02-28 11:36:34 +0700

    allow user can create account from 6 to 12 characters, account has below 6 charaters shoudl be bid through auction

--- contracts/eosio.system/src/eosio.system.cpp
+++ contracts/eosio.system/src/eosio.system.cpp
@@ -332,9 +332,12 @@ namespace eosiosystem {
            has_dot |= !(tmp & 0x1f);
            tmp >>= 5;
          }
+
+         bool has_below_6_chars = (newact.value & 0x3FFFFFFF0ull) == 0;
+
          if( has_dot ) { // or is less than 12 characters
             auto suffix = newact.suffix();
-            if( suffix == newact ) {
+            if( suffix == newact && has_below_6_chars) {
                check( false, "disable name auction");
                name_bid_table bids(get_self(), get_self().value);
                auto current = bids.find( newact.value );
@@ -342,7 +345,7 @@ namespace eosiosystem {
                check( current->high_bidder == creator, "only highest bidder can claim" );
                check( current->high_bid < 0, "auction for name is not closed yet" );
                bids.erase( current );
-            } else {
+            } else if (suffix != newact){
                check( creator == suffix, "only suffix may create this account" );
             }
          }
